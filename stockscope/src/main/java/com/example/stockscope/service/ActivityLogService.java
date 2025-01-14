2baf41d597b53367df2327cb1e8f9de6475ff6f3
asish <2200032056@kluniversity.in>
2025-01-14 21:46:03 +0530

    chore: remove extra stockscope folder

--- stockscope/src/main/java/com/example/stockscope/service/ActivityLogService.java
+++
@@ -1,7 +0,0 @@
-package com.example.stockscope.service;
-
-import com.example.stockscope.model.ActivityLog;
-
-public interface ActivityLogService {
-    void logActivity(String userId, ActivityLog.ActivityType type, String description, String additionalInfo);
-}
