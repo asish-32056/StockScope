dc626972ab18a05a1b62ed48d5ec4feacd9af9ed
asish <2200032056@kluniversity.in>
2025-01-20 16:55:24 +0530

    Updated configurations and fixed rate limiting

--- src/main/java/com/example/stockscope/exception/RateLimitExceededException.java
+++ src/main/java/com/example/stockscope/exception/RateLimitExceededException.java
@@ -1,4 +1,4 @@
-package com.example.stockscope.exception
+package com.example.stockscope.exception;
 
 public class RateLimitExceededException extends RuntimeException {
     public RateLimitExceededException(String message) {
